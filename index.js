23687112211984cb0cfbba336f9b630ae45607f3
markoswythor <76796419+markoswythor@users.noreply.github.com>
2021-01-08 10:35:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -333,7 +333,7 @@ async function starts() {
 				case 'dono':
 					memein = await kagApi.memeindo()
 					buffer = await getBuffer(`https://i.imgur.com/d1M6lOz.jpg`)
-					client.sendMessage(from, buffer, image, {quoted: mek, caption: '*CRIADOR:* Dark\n*YOUTUBE:* https://bit.ly/2MD43V2\n*WPP:* wa.me/+5517991134416\n*INSTA:* to sem aff kk\n\n\n*Digite *.bot* para ver comandos basicos para criar um bot'})
+					client.sendMessage(from, buffer, image, {quoted: mek, caption: '*CRIADOR:* Markos\n*YOUTUBE:* to sem\n*WPP:* wa.me/+5594988088531\n*INSTA:* to sem aff kk\n\n\n*Digite *.bot* para ver comandos basicos para criar um bot'})
 					break
 				case 'belle2':
 					memein = await kagApi.memeindo()
